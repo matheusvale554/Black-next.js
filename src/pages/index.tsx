3ab93732e3c7167e945e16a077c83700bb7ac3ee
francisco matheus <matheusvale554@gmail.com>
2024-01-15 20:53:54 -0300

    alteração pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,6 @@ const Home: NextPage = () => {
         <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint error consectetur modi odio obcaecati tempore dignissimos assumenda, eum reiciendis dolores magni autem at debitis voluptas. Rem ipsa deleniti autem dignissimos.
-        </p>
       </main>
     </>
   )
